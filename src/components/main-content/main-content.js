fbc4034774d12c7adea33030fa73fb5b9b0d9a34
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-29 17:05:59 -0600

    outlining component functions & component styling

---
+++ src/components/main-content/main-content.js
@@ -0,0 +1,15 @@
+// react import
+import React from 'react'
+import { Link } from "react-router-dom"
+
+// main function
+function Main() {
+    return (
+        <div className="main">
+
+        </div>
+    )
+}
+
+// react export
+export default Main
---
+++ src/components/search/search.js
@@ -0,0 +1,15 @@
+// react import
+import React from 'react'
+import { Link } from "react-router-dom"
+
+// search function
+function Search() {
+    return (
+        <div className="search">
+
+        </div>
+    )
+}
+
+// react export
+export default Search
