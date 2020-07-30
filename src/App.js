1416b21d421e8d15ed661883c0d9a96c1c4a9887
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-07-29 20:19:45 -0600

    Added imports, paths

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import React from 'react';
 // import Nav from './components/nav/nav'
 import Nav from './components/nav/nav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from "./pages/home";
-import Addrecipe from "./pages/add-recipe";
-import Search from "./pages/search";
-import Logout from "./pages/logout"
+import { About, Addrecipe, Contact, Editrecipe, Favorites, Help, Home, Logout, Profile, Search, Settings, Viewrecipe } from "./pages/index"
 
-
-// 
+// import Addrecipe from "./pages/add-recipe"
 function App() {
   return (
     <Router>
@@ -18,14 +14,17 @@ function App() {
         <Nav/>
         <Switch>
           <Route exact path= '/' component= {Home}/> 
-
           <Route exact path= '/addrecipe' component= {Addrecipe}/> 
           <Route exact path= '/search' component= {Search}/> 
           <Route exact path= '/logout' component= {Logout}/> 
-          {/* <Route exact path= '/' component= {User-Profile}/> 
-          <Route exact path= '/' component= {Favorites}/> 
-          <Route exact path= '/' component= {Settings}/> 
-          <Route exact path= '/' component= {Help}/>  */}
+          <Route exact path= '/profile' component= {Profile}/> 
+          <Route exact path= '/favorites' component= {Favorites}/> 
+          <Route exact path= '/settings' component= {Settings}/> 
+          <Route exact path= '/help' component= {Help}/>
+          <Route exact path= '/about' component= {About}/>
+          <Route exact path= '/contact' component= {Contact}/>
+          <Route exact path= '/editrecipe' component= {Editrecipe}/>
+          <Route exact path= '/viewrecipe' component= {Viewrecipe}/>
         </Switch>
       </div>
     </Router>
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import About from "./about"
+import Contact from "./contact"
+import Editrecipe from "./edit-recipe"
+import Favorites from "./favorites"
+import Help from "./help"
+import Settings from "./settings"
+import Viewrecipe from "./search"
+import Home from "./home";
+import Addrecipe from "./add-recipe.js";
+import Search from "./search.js";
+import Logout from "./logout";
+import Profile from "./profile";
+
+export {
+    About,
+    Contact,
+    Editrecipe,
+    Favorites,
+    Help,
+    Settings,
+    Viewrecipe,
+    Home,
+    Addrecipe,
+    Search,
+    Logout,
+    Profile
+}
