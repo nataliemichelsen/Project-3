29b81034e57aecf3bcaa8a43c8df5fc0267b04aa
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-07-29 20:05:11 -0600

    It works now

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import Container from "../../components/container";
-import Column from "../../components/column";
-import Row from "../../components/row";
+
 
 function About() {
   return (
