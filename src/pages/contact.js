1f2cec4bd7c273edd7c6f1ebfc2b96b125f03de5
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-29 21:14:46 -0600

    pages styling outline & pages filled out

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,19 +4,33 @@ import React, { useState } from "react";
 function Contact() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
+      <h1>Submit a form below to get in touch.</h1>
     </div>
-  );
+      );
 }
+
+  // for contact form, needs edit
+  class Form extends Component {
+    // Setting the component's initial state
+    state = {
+      firstName: "",
+      lastName: "",
+      password: ""
+    };
+  
+    handleInputChange = event => {
+      // Getting the value and name of the input which triggered the change
+      let value = event.target.value;
+      const name = event.target.name;
+  
+      if (name === "password") {
+        value = value.substring(0, 15);
+      }
+      // Updating the input's state
+      this.setState({
+        [name]: value
+      });
+    };
+
 
 export default Contact;
