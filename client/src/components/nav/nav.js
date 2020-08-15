a519cf58722fffccb95288716ddd9c789591b2ea
Ahmed Jalal <jalal.ahmed@orbcomm.com>
2020-08-15 12:50:47 -0600

    navs

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,36 +1,186 @@
 // react import
 import React from "react";
-import {Navbar, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 // nav function
 // must still add svg files "xmlns" for vector art
 function Nav() {
   return (
-<>
-    <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">ReciPique</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Add Recipes</Nav.Link>
-      <Nav.Link href="#link">Search Recipes</Nav.Link>
-      <Nav.Link href="#link">Logout</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Favorites</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Settings</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.4">Help</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
-  </>
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      style={{ backgroundColor: "#333333" }}
+    >
+      <span id="header" className="navbar-brand mb-1 h1">
+        ReciPique
+      </span>
+      <button
+        id="navbarbutton"
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul id="items" className="nav navbar-nav navbar-right">
+          <li className="navbar">
+            <Link className="nav-link" to="/" style={{ color: "#333333" }}>
+              Home
+            </Link>
+            <svg
+              id="add"
+              className="nav-btn mx-auto bi bi-plus-square"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns=""
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+              />
+            </svg>
+          </li>
+          <li className="navbar">
+            <Link
+              className="nav-link"
+              to="/add-recipe"
+              style={{ color: "#333333" }}
+            >
+              Add Recipe
+            </Link>
+            <svg
+              id="add"
+              className="nav-btn mx-auto bi bi-plus-square"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns=""
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+              />
+            </svg>
+          </li>
+          <li className="navbar">
+            <Link
+              className="nav-link"
+              to="/search"
+              style={{ color: "#333333" }}
+            >
+              Search Recipes
+            </Link>
+            <svg
+              id="add"
+              className="nav-btn mx-auto bi bi-plus-square"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns=""
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+              />
+            </svg>
+          </li>
+          <li className="navbar">
+            <Link
+              className="nav-link"
+              to="/logout"
+              style={{ color: "#333333" }}
+            >
+              Logout
+            </Link>
+            <svg
+              id="add"
+              className="nav-btn mx-auto bi bi-plus-square"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns=""
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+              />
+            </svg>
+          </li>
+        </ul>
+      </div>
+      <div className="dropdown">
+        <Link
+          id="linkButton"
+          className="btn btn-secondary dropdown-toggle"
+          to="#"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          *
+        </Link>
+        <div
+          id="dropdown-bar"
+          className="dropdown-menu"
+          aria-labelledby="dropdownMenuLink"
+        >
+          <Link id="dropdown-items" className="dropdown-item" to="/profile">
+            User Profile
+          </Link>
+          <Link id="dropdown-items" className="dropdown-item" to="/favorites">
+            Saved Recipes
+          </Link>
+          <Link id="dropdown-items" className="dropdown-item" to="/settings">
+            Settings
+          </Link>
+          <Link id="dropdown-items" className="dropdown-item" to="/help">
+            Help
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 }
 
