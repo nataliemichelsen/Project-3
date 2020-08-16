4a6e9b9f9a773d291e32b15d51812e98a2ea1bdb
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-08-16 12:33:49 -0600

    Dropdown menu now working

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
