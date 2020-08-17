c75c9ce26551ca7f5b42a15f8eb3e331413efe50
Ahmed Jalal <jalal.ahmed@orbcomm.com>
2020-08-16 20:26:00 -0600

    fixed upload

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const session = require('cookie-session')
-// passport configuration
+const session = require("express-session");
 const passport = require("./config/passport");
 
 const routes = require("./routes");
@@ -8,44 +7,27 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// using sessions to keep track of our user's login status
-app.set('trust proxy', 1)
 app.use(
   session({ 
-    secret: "keyboard cat", 
+    secret: "Hamoody", 
     name: 'session',
     resave: false, 
-    saveUninitialized: true,
-    keys: ['key1', 'key2'], 
-    cookie: {
-      secure: true,
-      httpOnly: true,
-      // domain: 'example.com',
-      // path: '/',
-      expires: 1000,
-      sameSite: true
-    }
+    saveUninitialized: true
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(bodyParser())
-//app.use(cookieParser())
-// Serve up static assets (usually on heroku)
-
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+
 app.use(routes);
 
-// Start the API server
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
